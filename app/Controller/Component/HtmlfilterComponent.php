08554140e4f20e3cb6221b82f2e1a5ea69e8ded3
electblake <electblake@gmail.com>
2013-02-17 22:39:17 -0500

    fixed htmlfilter

--- app/Controller/Component/HtmlfilterComponent.php
+++ app/Controller/Component/HtmlfilterComponent.php
@@ -1,16 +1,22 @@
 <?
 	class HtmlfilterComponent extends Object {
 		public $htmlFilter;
+		public $controller;
 		/**
 		 * Import the HTML Filter vendor files and instantiate the object.
 		 */
-		public function __construct() {
+		public function initialize($controller) {
 			
 			/**
 			 * Import the Markdownify vendor files.
 			 */
-			App::import('Vendor', 'htmlfilter/htmlfilter');
-			$this->htmlFilter = new HtmlFilter;
+			//App::import('Vendor', 'htmlfilter/htmlfilter');
+			//$this->htmlFilter = new HtmlFilter;
+			
+
+			App::uses('HtmlFilter', 'Vendor/HtmlFilter', array('file' => 'htmlfilter.php'));
+			$this->controller = $controller;
+			$this->controller->htmlFilter = new HtmlFilter;
 		}
 		
 		public function filter($content) {
